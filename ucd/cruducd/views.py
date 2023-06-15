7e4f1921486be5925274623e87ab6f29f4aaea7a
Slb59 <sylvie.bricout2@laposte.net>
2023-06-15 17:26:44 +0200

    add story views

--- ucd/cruducd/views.py
+++ ucd/cruducd/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Story
 
-# Create your views here.
+
+class StoryBaseView(View):
+    model = Story
+    fields = '__all__'
+    success_url = reverse_lazy('crudocd:all')
+
+
+class StoryListView(StoryBaseView, ListView):
+    """ """
+
+
+class StoryDetailView(StoryBaseView, DetailView):
+    """ """
+
+
+class StoryCreateView(StoryBaseView, CreateView):
+    """ """
+
+
+class StoryUpdateView(StoryBaseView, UpdateView):
+    """ """
+
+
+class StoryDeleteView(StoryBaseView, DeleteView):
+    """ """
