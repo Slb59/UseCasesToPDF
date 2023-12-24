d4c948b176d0295ab1f1f423c177e0133799c52d
Sylvie <sylvie.bricout2@laposte.net>
2023-12-24 10:12:14 +0100

    add merge tool

---
+++ utils/merge.py
@@ -0,0 +1,16 @@
+from pypdf import PdfMerger
+
+pdfs = ['utils/files/p1-2.pdf', 'utils/files/p3.pdf', 'utils/files/p4.pdf', 'utils/files/p5.pdf']
+pdfs.append('utils/files/p6-15.pdf')
+pdfs.append('utils/files/p16.pdf')
+pdfs.append('utils/files/p17.pdf')
+pdfs.append('utils/files/p18.pdf')
+pdfs.append('utils/files/p19.pdf')
+
+merger = PdfMerger()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("utils/files/result.pdf")
+merger.close()
